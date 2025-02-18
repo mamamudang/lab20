c6f50b97b83e3a6385c6a198c2ac39dbfdc45d8c
mamamudang <humlake7.66@gmail.com>
2025-02-18 14:53:22 +0700

    lab20.h completed

--- lab20.h
+++ lab20.h
@@ -7,54 +7,66 @@
 
 using namespace std;
 
-class Equipment{
-	int hpmax;
-	int atk;
-	int def;
-	public:
-		Equipment(int,int,int);
-		vector<int> getStat();			
+class Equipment {
+    int hpmax;
+    int atk;
+    int def;
+public:
+    Equipment(int, int, int);
+    vector<int> getStat();
 };
 
-class Unit{
-		string name;
-		string type;		
-		int hp;
-		int hpmax;
-		int atk;
-		int def;
-		bool guard_on;
-		bool dodge_on; 
-		Equipment *equipment; 
-	public:			
-		Unit(string,string); 
-		void showStatus();
-		void newTurn();
-		int attack(Unit &);
-		int ultimateAttack(Unit &); 
-		int beAttacked(int);
-		int heal();	
-		void guard();
-		void dodge(); 
-		bool isDead();
-		void equip(Equipment *);  
+Equipment::Equipment(int h, int a, int d) {
+    hpmax = h;
+    atk = a;
+    def = d;
+}
+
+vector<int> Equipment::getStat() {
+    return {hpmax, atk, def};
+}
+
+class Unit {
+    string name;
+    string type;
+    int hp;
+    int hpmax;
+    int atk;
+    int def;
+    bool guard_on;
+    bool dodge_on;
+    Equipment *equipment;
+
+public:
+    Unit(string, string);
+    void showStatus();
+    void newTurn();
+    int attack(Unit &);
+    int ultimateAttack(Unit &);
+    int beAttacked(int);
+    int heal();
+    void guard();
+    void dodge();
+    bool isDead();
+    void equip(Equipment *);
 };
 
-Unit::Unit(string t,string n){ 
-	type = t;
-	name = n;
-	if(type == "Hero"){
-		hpmax = rand()%20+120;
-		atk = rand()%5+14;
-		def = rand()%3+9;
-	}else if(t == "Monster"){
-		hpmax = rand()%20+250;
-		atk = rand()%5+25;
-		def = rand()%3+5;
-	}
-	hp = hpmax;	
-	guard_on = false;
-	equipment = NULL;
+Unit::Unit(string t, string n) {
+    type = t;
+    name = n;
+    if (type == "Hero") {
+        hpmax = rand() % 20 + 120;
+        atk = rand() % 5 + 14;
+        def = rand() % 3 + 9;
+    } else if (t == "Monster") {
+        hpmax = rand() % 20 + 250;
+        atk = rand() % 5 + 25;
+        def = rand() % 3 + 5;
+    }
+    hp = hpmax;
+    guard_on = false;
+    dodge_on = false;
+    equipment = NULL;
 }
 
 void Unit::showStatus(){
@@ -72,40 +84,49 @@ void Unit::showStatus(){
 	}
 }
 
-void Unit::newTurn(){
-	guard_on = false; 
+void Unit::newTurn() {
+    guard_on = false;
+    dodge_on = false;
 }
 
-int Unit::beAttacked(int oppatk){
-	int dmg;
-	if(oppatk > def){
-		dmg = oppatk-def;	
-		if(guard_on) dmg = dmg/3;
-	}	
-	hp -= dmg;
-	if(hp <= 0){hp = 0;}
-	
-	return dmg;	
+int Unit::beAttacked(int oppatk) {
+    int dmg = 0;
+    if (dodge_on && rand() % 2 == 0) {
+        dmg = 0;
+    } else {
+        if (oppatk > def) {
+            dmg = oppatk - def;
+            if (guard_on) dmg /= 3;
+            if (dodge_on) dmg *= 2;
+        }
+    }
+    hp -= dmg;
+    if (hp <= 0) hp = 0;
+
+    return dmg;
 }
 
-int Unit::attack(Unit &opp){
-	return opp.beAttacked(atk);
+int Unit::attack(Unit &opp) {
+    return opp.beAttacked(atk);
 }
 
-int Unit::heal(){
-	int h = rand()%21 + 10;
-	if(hp + h > hpmax) h = hpmax - hp;
-	hp = hp + h;
-	return h;
+int Unit::ultimateAttack(Unit &opp) {
+    return opp.beAttacked(atk * 2);
 }
 
-void Unit::guard(){
-	guard_on = true;
-}	
+int Unit::heal() {
+    int h = rand() % 21 + 10;
+    if (hp + h > hpmax) h = hpmax - hp;
+    hp = hp + h;
+    return h;
+}
 
-bool Unit::isDead(){
-	if(hp <= 0) return true;
-	else return false;
+void Unit::guard() {
+    guard_on = true;
+}
+
+void Unit::dodge() {
+    dodge_on = true;
 }
 
 void drawScene(char p_action,int p,char m_action,int m){
@@ -150,6 +171,23 @@ void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
 };
 
+void Unit::equip(Equipment *newEquip) {
+    if (equipment != NULL) {
+        vector<int> oldStats = equipment->getStat();
+        hpmax -= oldStats[0];
+        atk -= oldStats[1];
+        def -= oldStats[2];
+    }
+    
+    vector<int> newStats = newEquip->getStat();
+    hpmax += newStats[0];
+    atk += newStats[1];
+    def += newStats[2];
+    
+    if (hp > hpmax) hp = hpmax;
+    
+    equipment = newEquip;
+}
 
 void playerWin(){	
 	cout << "*******************************************************\n";
